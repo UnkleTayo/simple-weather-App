bda6e75cb53fb6aab9850a9723366388759a07bd
Adetayo <akinsanyaadetayo7@gmail.com>
2020-06-29 13:41:23 +0100

    Add UpdateUI func to update the ui

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,24 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+  const { cityDets, weather } = data;
+  // update details template
+
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric}</span>
+      <span>&deg;C</span>
+    </div>
+
+  `;
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
 
 const updateCity = async (city) => {
   const cityDets = await getCity(city);
@@ -19,6 +39,6 @@ cityForm.addEventListener('submit', (e) => {
 
   // Update UI with new City
   updateCity(city)
-    .then((data) => console.log(data))
+    .then((data) => updateUI(data))
     .catch((err) => console.log(err));
 });
